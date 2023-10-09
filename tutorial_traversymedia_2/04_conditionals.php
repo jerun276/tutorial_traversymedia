bf7731926d60ea84c3903198944e5cda42896591
jerome <zachariah11jerobert@gmail.com>
2023-10-09 13:51:53 +0530

    code for conditional

---
+++ tutorial_traversymedia_2/04_conditionals.php
@@ -0,0 +1,11 @@
+<?php
+/*
+    < Less than
+    > Greater than
+    <= Less than or equal to 
+    >= Greater than or equal to
+    == Equal to
+    === Identical to
+    != Not equal to
+    !== Not identical to
+*/
